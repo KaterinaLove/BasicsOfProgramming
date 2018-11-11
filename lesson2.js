cce1f420e401935f7b378044a6dfcb0148031414
KaterinaLove <KaterinaLove@users.noreply.github.com>
2018-11-11 18:32:46 +0300

    Сделала 4 задание

--- lesson2.js
+++ lesson2.js
@@ -35,3 +35,43 @@ while (year > 0) {
 console.log("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
 console.log("Задание 3");
 console.log("Встроить в сайт свою фотографию");
+
+console.log("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+console.log("Задание 4");
+console.log("Игра для 2 играков");
+var compNum Math.random(Math.random() * 100);
+
+//Диалоговое окошко
+if (confirm('Вы хотите начать игру?')) {
+
+  var gameOver = false, 
+      areLosers = true;
+//первый цикл для 10 раундов
+for (var i = 0; i < 10; i++) {
+  //второй цикл для количества икраков
+  for (var j = 0; j < 2; j++) {
+    var input = +prompt('Раунд ' + (i + 1) + '\nИгрок ' + (j + 1) + ', введите число \nВведите end для окончания инры');
+    if (input == 'end') {
+      gameOver = true;
+      break;
+    }
+    var userNum = +input;
+    if (userNum < compNum) {
+      alert('Число меньше загаданного');
+    } else if (userNum > compNum) {
+      alert('Число больше заганного');
+    } else if ( userNum == compNum) {
+      alert('Игрок ' + (j + 1) + ' угадал! Игра закончила');
+      gameOver = true;
+      areLosers = false;
+      break;
+    } else {alert('Ошибка')}
+  }
+  if (gameOver) {
+    break;
+  }
+}
+  if (areLosers && gameOver) {
+    alert('Вы не угодали число за нужное количество раундов');
+  }
+}
